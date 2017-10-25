330f33ceb9fe69b9c897786dde9fc4eaf8e15971
Alvaro Brito <alvaro.brito@ontruck.com>
2017-10-25 20:13:06 +0200

    intl-loader support for node < 8

--- packages/react-scripts/config/ontruck-react-scripts/loaders/intl-loader/transformer.js
+++ packages/react-scripts/config/ontruck-react-scripts/loaders/intl-loader/transformer.js
@@ -12,15 +12,9 @@ const transformJSON = (obj, base = '') => (
   Object.keys(obj).reduce((acc, key) => {
     const newBase = base ? `${base}.${key}` : key;
     if (typeof obj[key] === 'object') {
-      return {
-        ...acc,
-        ...transformJSON(obj[key], newBase)
-      };
+      return Object.assign(acc, transformJSON(obj[key], newBase));
     }
-    return {
-      ...acc,
-      [newBase]: obj[key]
-    };
+    return Object.assign(acc, { [newBase]: obj[key] });
   }, {})
 );
 
