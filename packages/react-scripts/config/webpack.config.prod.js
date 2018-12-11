549f1b9573ab8f16548c46f3d93c1d960e656942
gerardkabre <gerard.kabre@gmail.com>
2018-12-11 13:50:14 +0100

    install worker-loader in react-scripts for webWorkers

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -289,6 +289,10 @@ module.exports = {
               parentId: `${process.env.SVG_ICONS_PARENT_ID}`,
             },
           },
+          {
+            test: /\.worker\.js$/,
+            use: { loader: require.resolve('worker-loader') },
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
